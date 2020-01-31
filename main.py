d16fdf6de336579aef7a538441bc5619eb0d3da4
Penloy <lloydscottschool@gmail.com>
2020-01-31 22:17:17 +0000

    Added some more stuff

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import discord
 import os
-from discord.ext import commands
+from discord.ext import commands, tasks
+from itertools import cycle
 
 bot = commands.Bot(command_prefix = '.')
+status = cycle(['Communism', 'With Stalin', 'and Chilling'])
+
+#When the bot starts
+@bot.event
+async def on_ready():
+    change_status.start()
 
 @bot.command()
 async def load(ctx, extension):
@@ -24,5 +31,9 @@ async def reload(ctx, extension):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
+
+@tasks.loop(seconds=10)
+async def change_status():
+    await bot.change_presence(activity=discord.Game(next(status)))
 
 bot.run('NjcyODY5MjQyOTkwODg2OTMy.XjSIEA.hsLLfKuThXEpaVQQpLkqhQAU8Q0')
