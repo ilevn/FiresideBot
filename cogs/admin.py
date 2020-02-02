e2373e1baaf2a50b5f3a306db9303fd9a040338e
Nils T <nilsntth@gmail.com>
2020-02-03 12:09:57 +1300

    Add docs to admin commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,4 +1,5 @@
 import time
+import traceback
 
 from discord.ext import commands
 from discord.ext.commands import is_owner
@@ -8,27 +9,32 @@ from cogs.utils.meta_cog import Cog
 
 class Admin(Cog):
     @commands.command()
-    async def ping(self, ctx):
-        before = time.monotonic()
-        msg = await ctx.send(":ping_pong: Pong!")
-        after = time.monotonic()
-        await msg.edit(content=f":ping_pong: Pong! | {round((after - before) * 1000, 2)}ms")
-
-    @commands.command()
     @is_owner()
     async def load(self, ctx, *, extension):
-        ctx.bot.load_extension(f"cogs.{extension}")
-        await ctx.send(f"{extension} loaded")
+        """Load an extension"""
+        try:
+            ctx.bot.load_extension(f"cogs.{extension}")
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.send(f"Could not load `{extension}` -> `{e}`")
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
 
     @commands.command()
     @is_owner()
     async def unload(self, ctx, extension):
-        ctx.bot.unload_extension(f"cogs.{extension}")
-        await ctx.send(f"{extension} unloaded")
+        """Unload an extension."""
+        try:
+            ctx.bot.unload_extension(f"cogs.{extension}")
+        except commands.ExtensionError as e:
+            await ctx.send(f"{e.__class__.__name__}: {e}")
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
 
     @commands.command()
     @is_owner()
     async def reload(self, ctx, *, extension):
+        """Reload an extension. """
         try:
             ctx.bot.reload_extension(extension)
         except commands.ExtensionError as e:
