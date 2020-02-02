ca081c6c006ca0a79371e9945f48005faa12bdc6
Nils T <nilsntth@gmail.com>
2020-02-03 12:11:48 +1300

    Add meta cog with custom help command, ping and charinfo.

---
+++ cogs/meta.py
@@ -0,0 +1,44 @@
+import time
+import unicodedata
+
+from discord.ext import commands
+
+from cogs.utils.meta_cog import Cog
+from cogs.utils.paginators import PaginatedHelpCommand
+
+
+class Meta(Cog):
+    """Commands for functions related to Discord or the bot itself."""
+
+    def __init__(self, bot):
+        super().__init__(bot)
+        self.old_help_command = bot.help_command
+        bot.help_command = PaginatedHelpCommand()
+        bot.help_command.cog = self
+
+    def cog_unload(self):
+        self.bot.help_command = self.old_help_command
+
+    @commands.command()
+    async def charinfo(self, ctx, *, characters: str):
+        """Shows information about a number of characters."""
+
+        def to_string(c):
+            digit = f'{ord(c):x}'
+            name = unicodedata.name(c, 'Name not found.')
+            return f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{digit}>'
+
+        msg = '\n'.join(map(to_string, characters))
+        if len(msg) > 2000:
+            return await ctx.send('Output too long to display.')
+        await ctx.send(msg)
+
+    @commands.command()
+    async def ping(self, ctx):
+        before = time.monotonic()
+        msg = await ctx.send(":ping_pong: Pong!")
+        after = time.monotonic()
+        await msg.edit(content=f":ping_pong: Pong! | {round((after - before) * 1000, 2)}ms")
+
+
+setup = Meta.setup
