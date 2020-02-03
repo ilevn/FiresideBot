b4b659cfefc7db77e2ce547e3d0c7def04075884
Nils T <nilsntth@gmail.com>
2020-02-03 19:55:16 +1300

    Add `CannotPaginate` to paginators namespace

--- cogs/utils/paginators/__init__.py
+++ cogs/utils/paginators/__init__.py
@@ -1,4 +1,4 @@
-from .base_paginator import Pages
+from .base_paginator import Pages, CannotPaginate
 from .bulk_delete_paginator import BulkDeletePaginator
 from .help_paginator import HelpPaginator, PaginatedHelpCommand
 
