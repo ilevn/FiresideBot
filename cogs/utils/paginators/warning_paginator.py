9d6a0a82524a4e5f6de95f5825e94734928334dc
Nils T <nilsntth@gmail.com>
2020-03-26 19:42:45 +0100

    Add warnings cog

---
+++ cogs/utils/paginators/warning_paginator.py
@@ -0,0 +1,115 @@
+import itertools
+
+from cogs.utils import Plural
+from cogs.utils.paginators import Pages, CannotPaginate
+
+
+class WarningPaginator(Pages):
+    def __init__(self, ctx, entries, *, per_page=4):
+        super().__init__(ctx, entries=entries, per_page=per_page)
+        self.total = len(entries)
+
+    @staticmethod
+    def _format_desc(warnings, notes):
+        return f"{Plural(warnings):warning}, {Plural(notes):note}"
+
+    @classmethod
+    async def from_all(cls, ctx):
+        # Get all entries.
+        query = """SELECT id, information, created, warning, member_id, moderator_id
+                   FROM warning_entries
+                   WHERE guild_id = $1
+                   ORDER BY created DESC"""
+
+        records = await ctx.db.fetch(query, ctx.guild.id)
+        if not records:
+            raise CannotPaginate("No warnings or notes found.")
+
+        nested_pages = []
+        per_page = 8
+
+        def key(r):
+            return r["member_id"]
+
+        records = sorted(records, key=key)
+
+        # 0: <member>, <len(warnings) len(notes)>, <ID, text, created, warning type>
+        # 1: <member>, <len(warnings) len(notes)>, <ID, text, created, warning type>
+        # ...
+        for member_id, info in itertools.groupby(records, key=key):
+            member = ctx.guild.get_member(member_id)
+            if not member:
+                # Warned member left guild. Don't bother paginating.
+                continue
+
+            def key(r):
+                return not r["warning"]
+
+            info = sorted(info, key=key)
+            notes = sum(1 for _ in filter(key, info))
+            desc = cls._format_desc(len(info) - notes, notes)
+
+            needed_info = [r[0:4] + (getattr(ctx.guild.get_member(r[5]), 'name', 'Mod left'),) for r in info]
+            nested_pages.extend(
+                (str(member), desc, needed_info[i:i + per_page]) for i in range(0, len(needed_info), per_page))
+
+        self = cls(ctx, nested_pages, per_page=1)
+        self.get_page = self.get_member_page
+        self.total = sum(len(o) for _, _, o in nested_pages)
+        return self
+
+    @classmethod
+    async def from_member(cls, ctx, member, short_view=False):
+        query = f"""SELECT id, information, created, warning, moderator_id
+                    FROM warning_entries
+                    WHERE guild_id = $1
+                      AND member_id = $2
+                    ORDER BY created DESC
+                   {'LIMIT 4' if short_view else ''}"""
+
+        guild = ctx.guild
+        records = await ctx.db.fetch(query, guild.id, member.id)
+        if not records:
+            raise CannotPaginate(f"No warnings or notes found for {member}.")
+
+        # Sort by type actual warning > note.
+        info = sorted(records, key=lambda r: not r["warning"])
+
+        # Get number of warnings and notes on this member.
+        query = """SELECT count(*) FILTER (WHERE NOT warning) AS note,
+                          count(*) FILTER (WHERE warning)     AS warned
+                   FROM warning_entries
+                   WHERE guild_id = $1
+                   AND member_id = $2"""
+
+        notes, warnings = await ctx.db.fetchrow(query, guild.id, member.id)
+        self = cls(ctx, [r[0:4] + (getattr(guild.get_member(r[4]), 'name', 'Mod left'),) for r in info])
+        self.description = self._format_desc(warnings, notes)
+        self.title = f'Overview for {member}'
+        self.total = warnings + notes
+
+        return self
+
+    def get_member_page(self, page):
+        member, description, info = self.entries[page - 1]
+        self.title = f'Overview for {member}'
+        self.description = description
+        return info
+
+    def prepare_embed(self, entries, page, *, first=False):
+        self.embed.clear_fields()
+        self.embed.description = self.description
+        self.embed.title = self.title
+
+        self.embed.set_footer(text=f'Warning log')
+
+        for entry in entries:
+            # Get type.
+            type_ = "Warning" if entry[3] else "Note"
+
+            # Format the actual entry.
+            fmt = f"[{entry[0]}] {entry[2]:%d/%m/%Y} - {entry[1]} [*{entry[4]}*]"
+            self.embed.add_field(name=type_, value=fmt, inline=False)
+
+        if self.maximum_pages:
+            self.embed.set_author(name=f'Page {page}/{self.maximum_pages} ({self.total} DB entries)')
---
+++ cogs/warnings.py
@@ -0,0 +1,204 @@
+import asyncio
+from collections import namedtuple
+from typing import Union
+
+import discord
+from discord.ext import commands
+
+from cogs.utils import is_mod, db
+from cogs.utils.converters import FetchedUser
+from cogs.utils.meta_cog import Cog
+from cogs.utils.paginators import CannotPaginate
+from cogs.utils.paginators.warning_paginator import WarningPaginator
+
+
+class WarningEntry(db.Table, table_name="warning_entries"):
+    id = db.PrimaryKeyColumn()
+    # The member who was warned.
+    member_id = db.Column(db.Integer(big=True), nullable=False, index=True)
+    # The moderator who warned a member.
+    moderator_id = db.Column(db.Integer(big=True), nullable=False)
+    # The guild, in which a member was warned.
+    guild_id = db.Column(db.Integer(big=True), nullable=False)
+    # Information about the warning itself.
+    information = db.Column(db.String, nullable=False, index=True)
+    # Whether the warning is a note or an actual warning.
+    warning = db.Column(db.Boolean, default=True)
+    # When the warning was created.
+    created = db.Column(db.Datetime, default="now() at time zone 'utc'", index=True)
+
+
+def entry_id(arg):
+    try:
+        arg = int(arg)
+    except ValueError:
+        raise commands.BadArgument("Please supply a valid entry ID.")
+
+    if not 0 < arg < 2147483647:
+        raise commands.BadArgument("This looks like a member ID...")
+
+    return arg
+
+
+ActionInformation = namedtuple("ActionInformation", "member text is_warning")
+
+
+class Warnings(Cog):
+    """Central warning system of the bot."""
+
+    @commands.group(name="warn")
+    @is_mod()
+    async def warn(self, ctx):
+        """Central warning system of the bot."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help('warn')
+
+    @staticmethod
+    async def _interactive_invocation(ctx, member: discord.Member, is_warning=True):
+        # Delete invocation messages to make it less spammy
+        to_delete = [ctx.message]
+        try:
+            page = await WarningPaginator.from_member(ctx, member, short_view=True)
+            await page.paginate()
+            to_delete.append(page.message)
+        except CannotPaginate as e:
+            to_delete.append(await ctx.send(e))
+
+        to_delete.append(await ctx.send(f"Add a {'warning' if is_warning else 'note'} or type 'abort' to cancel."))
+
+        try:
+            info = await ctx.bot.wait_for("message", timeout=120,
+                                          check=lambda m: m.channel == ctx.channel and m.author == ctx.author)
+            to_delete.append(info)
+        except asyncio.TimeoutError:
+            await ctx.send("Took too long. Aborting...", delete_after=3)
+        else:
+            if info.content.lower() == 'abort':
+                await ctx.send("OK. Aborting...", delete_after=3)
+                return
+
+            confirmed = await ctx.prompt(
+                f"Gotcha. Are you sure you want to proceed and {'issue a warning' if is_warning else 'take a note'}?")
+
+            if not confirmed:
+                await ctx.send("OK. Aborting...", delete_after=3)
+                return
+
+            return info.clean_content
+        finally:
+            try:
+                await ctx.channel.delete_messages(to_delete)
+            except discord.HTTPException:
+                pass
+
+    async def take_action(self, ctx, information: ActionInformation):
+        text = information.text
+        member = information.member
+        type_ = "warning" if information.is_warning else "note"
+
+        if not text:
+            # Interactive warning.
+            text = await self._interactive_invocation(ctx, information.member, information.is_warning)
+            if not text:
+                return
+        else:
+            await ctx.message.delete()
+
+        query = """INSERT INTO warning_entries (member_id, moderator_id, guild_id, information, warning)
+                   VALUES ($1, $2, $3, $4, $5) RETURNING id"""
+
+        status = await ctx.db.fetchval(query, member.id, ctx.author.id, ctx.guild.id, text, information.is_warning)
+        await ctx.send(f"OK. Added new {type_} for {member}", delete_after=3)
+
+        cog = ctx.bot.get_cog("Event")
+        if not cog:
+            return
+
+        config = await cog.get_guild_config(ctx.guild.id)
+        if not config and config.mod_channel:
+            return
+
+        embed = discord.Embed(title=f"\U00002139 {ctx.author} created a new {type_}")
+        embed.add_field(name="Member", value=f"{member} (`{member.id}`)", inline=False)
+        embed.add_field(name="Content", value=text, inline=False)
+        embed.set_footer(text=f'Entry ID {status}')
+
+        await config.mod_channel.send(embed=embed)
+
+    @warn.command(name="issue")
+    async def warn_issue(self, ctx, member: discord.Member, *, text: str = None):
+        """Creates a warning for a user.
+        Providing text skips all database checks and straight up adds a new entry."""
+        await self.take_action(ctx, ActionInformation(member, text, is_warning=True))
+
+    @warn.command(name="note")
+    async def warn_note(self, ctx, member: discord.Member, *, text: str = None):
+        """Adds a note for a user.
+        Providing text skips all database checks and straight up adds a new entry."""
+        await self.take_action(ctx, ActionInformation(member, text, is_warning=False))
+
+    @staticmethod
+    async def get_info(id_, ctx):
+        # Used to double-check entries before editing/deleting them.
+        record = await ctx.db.fetchrow("SELECT information, moderator_id, member_id "
+                                       "FROM warning_entries WHERE id = $1", id_)
+        if not record:
+            raise RuntimeError
+
+        mod = ctx.guild.get_member(record[1])
+        user = ctx.guild.get_member(record[2]) or f"Left server ({record[2]})"
+
+        information = f"Responsible mod: {mod}\nMember: {user}\nText: {record[0]}"
+        return information
+
+    @warn.command(name="edit")
+    async def warn_edit(self, ctx, id: entry_id, *, text: str):
+        """Edits a warning or note.
+        A double-check is performed to prevent accidents.
+        Please inform other moderators before editing their issued warnings or notes."""
+        try:
+            information = await self.get_info(id, ctx)
+        except RuntimeError:
+            return await ctx.send("No entry matching this ID found.")
+
+        check = await ctx.prompt(
+            f"Are you sure you want to edit entry #{id}?\n\nAvailable information:\n{information}")
+
+        if not check:
+            return await ctx.send("Aborting...")
+
+        query = "UPDATE warning_entries SET information = $1 WHERE id = $2"
+        await ctx.db.execute(query, text, id)
+        await ctx.send("Successfully edited entry.")
+
+    @warn.command(name="delete")
+    async def warn_delete(self, ctx, id: entry_id):
+        """Deletes a warning or note.
+        A double-check is performed to prevent accidents.
+        Please inform other moderators before deleting their issued warnings or notes."""
+        try:
+            information = await self.get_info(id, ctx)
+        except RuntimeError:
+            return await ctx.send("No entry matching this ID found.")
+
+        check = await ctx.prompt(
+            f"Are you sure you want to delete entry #{id}?\nAvailable information:\n\n{information}")
+
+        if not check:
+            return await ctx.send("Aborting...")
+
+        query = "DELETE FROM warning_entries WHERE id = $1"
+        await ctx.db.execute(query, id)
+        await ctx.send("Successfully deleted entry.")
+
+    @warn.command(name="list")
+    async def warn_list(self, ctx, member: Union[discord.Member, FetchedUser] = None):
+        """Shows all warnings and notes for a member or the server if no member is provided."""
+        try:
+            page = await WarningPaginator.from_member(ctx, member) if member else await WarningPaginator.from_all(ctx)
+            await page.paginate()
+        except CannotPaginate as e:
+            await ctx.send(e)
+
+
+setup = Warnings.setup
