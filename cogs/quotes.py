48e43031adb077a9ca34615bc88e9361d2bdeae4
Nils T <nilsntth@gmail.com>
2020-04-01 00:25:33 +0200

    Add `quote all` command

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -8,6 +8,7 @@ from discord.ext.commands import clean_content
 from cogs.utils import db, is_mod
 from cogs.utils.converters import FetchedUser
 from cogs.utils.meta_cog import Cog
+from cogs.utils.paginators import FieldPages, CannotPaginate
 
 
 class QuoteConfig(db.Table, table_name="quotes"):
@@ -32,7 +33,6 @@ class Quotes(Cog):
         """Adds a quote for a specific user in this guild."""
         query = """INSERT INTO quotes (guild_id, user_id, quote) VALUES ($1, $2, $3)"""
         await ctx.db.execute(query, ctx.guild.id, member.id, quote)
-
         await ctx.send('\N{WHITE HEAVY CHECK MARK} Success :) Quote entry added')
 
     @_quote.command(name="random")
@@ -47,6 +47,41 @@ class Quotes(Cog):
         embed = discord.Embed(title=f"Here's a quote from {member_name}", colour=discord.Colour.light_grey())
         embed.description = textwrap.shorten(f"\N{PUBLIC ADDRESS LOUDSPEAKER}\n{quote}", width=2000)
         await ctx.send(embed=embed)
+
+    @_quote.command(name="all")
+    @is_mod()
+    async def quotes_all(self, ctx, *, member: discord.Member = None):
+        """Gets all quotes of a server member or the whole guild, if
+        no member is specified.
+        """
+        guild = ctx.guild
+        if not member:
+            subcheck = ""
+            args = (guild.id,)
+        else:
+            subcheck = "AND user_id = $2"
+            args = (guild.id, member.id)
+
+        query = f"SELECT quote, user_id FROM quotes WHERE guild_id = $1 {subcheck}"
+        records = await ctx.db.fetch(query, *args)
+
+        if not records:
+            await ctx.send("Could not find any quotes for this server...")
+
+        def get_member():
+            mem = guild.get_member(user_id)
+            return getattr(mem, "display_name", f"ID ({user_id})")
+
+        entries = []
+        for content, user_id in records:
+            header = get_member() if not member else "Content"
+            entries.append((header, content[:1048]))
+
+        pages = FieldPages(ctx, entries=entries)
+        try:
+            await pages.paginate()
+        except CannotPaginate as e:
+            await ctx.send(e)
 
 
 setup = Quotes.setup
