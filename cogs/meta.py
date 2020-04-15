e285ee8671d13468fec06fe4152e9fca7ae09b16
Nils T <nilsntth@gmail.com>
2020-04-15 21:41:43 +0200

    Add `dog` and `cat` commands

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,3 +1,4 @@
+import io
 import time
 import unicodedata
 from typing import Union
@@ -80,6 +81,39 @@ class Meta(Cog):
 
             js = await resp.json()
             await ctx.send(f"\N{DOG FACE} **Random dog fact:**\n{js['facts'][0]}")
+
+    @commands.command()
+    async def dog(self, ctx):
+        """Gives you a random dog."""
+        async with ctx.session.get("https://random.dog/woof") as resp:
+            if resp.status != 200:
+                return await ctx.send("No dog found :(")
+
+            filename = await resp.text()
+            url = f"https://random.dog/{filename}"
+            filesize = ctx.guild.filesize_limit if ctx.guild else 8388608
+            if filename.endswith((".mp4", ".webm")):
+                async with ctx.typing():
+                    async with ctx.session.get(url) as other:
+                        if other.status != 200:
+                            return await ctx.send("Could not download dog video :(")
+
+                        if int(other.headers["Content-Length"]) >= filesize:
+                            return await ctx.send(f"Video was too big to upload... Watch it here: {url} instead.")
+
+                        fp = io.BytesIO(await other.read())
+                        await ctx.send(file=discord.File(fp, filename=filename))
+            else:
+                await ctx.send(embed=discord.Embed(title='Random Dog').set_image(url=url))
+
+    @commands.command(hidden=True)
+    async def cat(self, ctx):
+        """Gives you a random cat."""
+        async with ctx.session.get("https://api.thecatapi.com/v1/images/search") as resp:
+            if resp.status != 200:
+                return await ctx.send('No cat found :(')
+            js = await resp.json()
+            await ctx.send(embed=discord.Embed(title="Random Cat").set_image(url=js[0]["url"]))
 
     @commands.command(aliases=["avatar"])
     async def avy(self, ctx, *, user: Union[discord.Member, FetchedUser] = None):
