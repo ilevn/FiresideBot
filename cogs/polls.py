9f6580610c1feb4570143c52ac7e6086d74c325a
Nils T <nilsntth@gmail.com>
2020-05-22 13:05:16 +0200

    Add support for poll entry edits

--- cogs/polls.py
+++ cogs/polls.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 
 from cogs.utils import db, is_mod
 from cogs.utils.cache import cache
+from cogs.utils.converters import entry_id
 from cogs.utils.meta_cog import Cog
 
 
@@ -17,6 +18,18 @@ class PollConfig(db.Table, table_name="polls"):
     channel_id = db.DiscordIDColumn(unique=True)
     # Whether the poll should be strictly moderated, true by default.
     is_strict = db.Column(db.Boolean, default=True)
+
+
+class PollEntry(db.Table, table_name="poll_entries"):
+    id = db.PrimaryKeyColumn()
+    # The message ID of the entry.
+    message_id = db.DiscordIDColumn()
+    # The author of the entry.
+    author_id = db.DiscordIDColumn(index=True)
+    # The channel this entry was created in.
+    channel_id = db.DiscordIDColumn(nullable=False)
+    # The guild this poll entry was created in.
+    guild_id = db.DiscordIDColumn(nullable=False)
 
 
 Poll = namedtuple("Poll", "channel_id is_strict")
@@ -39,6 +52,31 @@ class Polls(Cog):
         records = await self.bot.pool.fetch(query, guild_id)
         return records and {channel_id: Poll(channel_id, is_strict) for channel_id, is_strict in records}
 
+    async def create_poll(self, message):
+        author = message.author
+
+        embed = discord.Embed(colour=discord.Colour.blurple())
+        embed.set_author(name=author.name, icon_url=author.avatar_url)
+        content = message.clean_content[6:]
+        # Check if a mod mentioned someone.
+        mentions = None
+        if author.guild_permissions.manage_guild and message.role_mentions:
+            mentions = ", ".join(map(lambda r: r.mention, message.role_mentions))
+
+        # Add entry to DB.
+        query = "INSERT INTO poll_entries (author_id, channel_id, guild_id) VALUES ($1, $2, $3) RETURNING id"
+        entry_id = await self.bot.pool.fetchval(query, author.id, message.channel.id, message.guild.id)
+
+        embed.description = f"__**Poll:**__ {content}"
+        embed.set_footer(text=f'Entry ID {entry_id} | Edit this poll with "poll edit {entry_id} <new content>"')
+
+        new_message = await message.channel.send(content=mentions, embed=embed)
+        for emote in self.poll_emotes:
+            await new_message.add_reaction(emote)
+
+        await self.bot.pool.execute("UPDATE poll_entries SET message_id = $1 WHERE id = $2",
+                                    new_message.id, entry_id)
+
     @Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.guild is None:
@@ -58,18 +96,7 @@ class Polls(Cog):
         if message.content.lower().startswith("poll: "):
             await message.delete()
             # Make a new poll
-            embed = discord.Embed(colour=discord.Colour.blurple())
-            embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
-            content = message.clean_content[6:]
-            # Check if a mod mentioned someone.
-            mentions = None
-            if message.author.guild_permissions.manage_guild and message.role_mentions:
-                mentions = ", ".join(map(lambda r: r.mention, message.role_mentions))
-
-            embed.description = f"__**Poll:**__ {content}"
-            new_message = await message.channel.send(content=mentions, embed=embed)
-            for emote in self.poll_emotes:
-                await new_message.add_reaction(emote)
+            await self.create_poll(message)
             return
 
         if poll.is_strict:
@@ -77,7 +104,7 @@ class Polls(Cog):
             await message.channel.send(fmt, delete_after=14)
             await message.delete(delay=12)
 
-    @commands.group(name="polls")
+    @commands.group(name="polls", aliases=["poll"])
     @is_mod()
     async def _polls(self, ctx):
         """Handles poll channel configuration for the server."""
@@ -102,6 +129,31 @@ class Polls(Cog):
         await ctx.db.execute(query, ctx.guild.id, channel.id)
         await ctx.send('\N{OK HAND SIGN}')
         self.get_guild_polls.invalidate(self, ctx.guild.id)
+
+    @_polls.command(name="edit")
+    async def _polls_edit(self, ctx, id: entry_id, *, new_content: commands.clean_content):
+        """Edits a poll entry that you own."""
+        query = "SELECT author_id, message_id, channel_id FROM poll_entries WHERE id = $1 AND guild_id = $2"
+        entry = await ctx.db.fetchrow(query, id, ctx.guild.id)
+        if entry is None:
+            return await ctx.send("Could not find a poll entry with that ID.")
+
+        if entry[0] != ctx.author.id:
+            return await ctx.send("This poll entry does not belong to you.")
+
+        # Retrieve the channel.
+        channel = ctx.guild.get_channel(entry["channel_id"])
+        if not channel:
+            return await ctx.send("Looks like the channel for this poll entry was deleted.")
+
+        message = await channel.fetch_message(entry["message_id"])
+        if not message:
+            return await ctx.send("Looks like the message for this poll entry was deleted.")
+
+        embed = message.embeds[0]
+        embed.description = new_content
+        await message.edit(embed=embed)
+        await ctx.send("Successfully edited poll entry.")
 
     @commands.command()
     @is_mod()
