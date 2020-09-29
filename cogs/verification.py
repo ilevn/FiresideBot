809a0586e72d2de4b9d5de80b1525840977bc693
Nils T <nilsntth@gmail.com>
2020-09-29 22:34:09 +0200

    Add support for reaction roles as part of the verification process

--- cogs/verification.py
+++ cogs/verification.py
@@ -1,11 +1,34 @@
+from collections import defaultdict
+
 import discord
 from discord.ext import commands
 
-from cogs.utils import is_maintainer
+from cogs.polls import to_emoji
+from cogs.utils import is_maintainer, db, cache, is_mod
 from cogs.utils.meta_cog import Cog
 
 
+class ReactionTable(db.Table, table_name="reaction_table"):
+    # Guilds are only supposed to have one reaction channel for now.
+    guild_id = db.DiscordIDColumn(nullable=False)
+    role_id = db.DiscordIDColumn(unique=True)
+    emote = db.Column(db.String, nullable=False)
+
+
 class Verification(Cog):
+    def __init__(self, bot):
+        super().__init__(bot)
+        # guild -> dict(emoji, role_id)
+        self.mappings = defaultdict(dict)
+
+    @cache.cache()
+    async def get_role_emote_mapping(self, guild_id):
+        query = "SELECT emote, role_id FROM reaction_table WHERE guild_id = $1"
+        async with self.bot.pool.acquire() as conn:
+            records = await conn.fetch(query, guild_id)
+
+            return records and {e: r_id for (e, r_id) in records}
+
     @commands.group()
     @is_maintainer()
     async def disclaimer(self, ctx):
@@ -51,19 +74,26 @@ class Verification(Cog):
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if str(payload.emoji) != '\N{SQUARED OK}':
-            return
-
-        channel = self.bot.get_channel(payload.channel_id)
-        if not isinstance(channel, discord.TextChannel):
-            return
-
         event_cog = self.bot.get_cog("Event")
-        config = event_cog and await event_cog.get_guild_config(payload.guild_id)
+        guild_id = payload.guild_id
+        config = event_cog and await event_cog.get_guild_config(guild_id)
         if not config:
             return
 
-        if payload.message_id != config.verification_message_id:
+        if config.verification_channel.id != payload.channel_id:
+            return
+
+        mappings = await self.get_role_emote_mapping(guild_id)
+        if not mappings:
+            return
+
+        try:
+            role_id = mappings[str(payload.emoji)]
+        except KeyError:
+            return
+
+        guild = self.bot.get_guild(guild_id)
+        if (role := guild and guild.get_role(role_id)) is None:
             return
 
         user = self.bot.get_user(payload.user_id)
@@ -71,11 +101,44 @@ class Verification(Cog):
             return
 
         # Upgrade our user
-        guild = self.bot.get_guild(payload.guild_id)
         member = guild.get_member(user.id)
-        await member.remove_roles(discord.Object(id=config.verification_role_id))
+        await member.edit(roles=[role])
         # Use internal method to get around having to fetch the message object.
         await self.bot.http.remove_reaction(payload.channel_id, payload.message_id, payload.emoji, user.id)
+
+    @commands.command()
+    @is_mod()
+    async def reaction_roles(self, ctx, *roles: discord.Role):
+        """Allows you to set the reaction roles for this server.
+        Up to 20 roles are allowed."""
+
+        guild_id = ctx.guild.id
+        event_cog = ctx.bot.get_cog("Event")
+        config = event_cog and await event_cog.get_guild_config(guild_id)
+        if not config:
+            await ctx.send("Could not find the guild config for this server.")
+            return
+
+        # Check if this server already has reaction roles set up.
+        has_roles = await ctx.db.fetchval("SELECT 1 FROM reaction_table WHERE guild_id = $1", guild_id)
+        if has_roles:
+            should_delete = await ctx.prompt("It looks like this server already has reaction roles. "
+                                             "Should they be deleted?")
+            if should_delete:
+                await ctx.db.execute("DELETE FROM reaction_table WHERE guild_id = $1", guild_id)
+                await ctx.send("Deleted! Please make sure to also delete the old reaction message later.")
+
+        react_roles = [(to_emoji(i), role) for i, role in enumerate(roles, 1)]
+        fmt = "\n".join(f"{key}: {role.name}" for key, role in react_roles)
+        message = await config.verification_channel.send(fmt)
+
+        for emoji, _ in react_roles:
+            await message.add_reaction(emoji)
+
+        to_insert = ((e, r.id, guild_id) for (e, r) in react_roles)
+        await ctx.send(":ok_hand:")
+        await ctx.db.copy_records_to_table("reaction_table", columns=("emote", "role_id", "guild_id"),
+                                           records=to_insert)
 
 
 setup = Verification.setup
